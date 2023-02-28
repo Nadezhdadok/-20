d98c6b457f4e29c4f1c79414a98837f6d41ac055
homew <homew@DESKTOP-NULQPNB>
2023-02-28 20:42:11 +0300

    Добавьте файлы проекта.

---
+++ Задача 20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача20
+{
+    class Program
+    {
+        delegate double MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = D;
+            double a = myDelegate(1);
+            Console.WriteLine(a);
+
+            myDelegate = S;
+            double b = myDelegate(1);
+            Console.WriteLine(b);
+
+            myDelegate = V;
+            double c = myDelegate(1);
+            Console.WriteLine(c);
+        }
+
+        static double D(double R) => 2 * Math.PI * R;
+        static double S(double R) => Math.PI * R * R;
+        static double V(double R) => (4 * Math.PI * R * R * R) / 3;
+
+    
+    }
+
+   
+
+}
+
+    
+   
+
